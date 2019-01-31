212f52d00133d8693fbf4eb45892d8188cd4bb14
Robin Lebhar <robin_lebhar@hotmail.fr>
2019-01-31 15:03:42 -0500

    Simple animation

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Animated } from "react-native";
+import FactCard from "./components/fact-card";
+import axios from "axios";
+
+const RANDOM_FACT_URL =
+  "http://randomuselessfact.appspot.com/random.json?language=en";
+
+export default class App extends Component {
+  componentWillMount() {
+    this.position = new Animated.ValueXY(0, 0);
+    Animated.spring(this.position, {
+      toValue: { x: 200, y: 300 }
+    }).start();
+  }
+  render() {
+    return (
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.square} />
+      </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    marginTop: 100
+  },
+  square: {
+    width: 100,
+    height: 100,
+    backgroundColor: "red"
+  }
+});
